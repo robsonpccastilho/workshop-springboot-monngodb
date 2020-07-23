b06db48055e5c75169e8c2e924b6fcfc089ea5d9
Robson Castilho <estudo@rcastilho.com.br>
2020-07-23 14:12:08 -0300

    Obtendo um post por id

---
+++ src/main/java/com/robsonpccastilho/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.robsonpccastilho.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.robsonpccastilho.workshopmongo.domain.Post;
+import com.robsonpccastilho.workshopmongo.repository.PostRepository;
+import com.robsonpccastilho.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Post --> Objeto não encontrado!"));
+	}
+}
