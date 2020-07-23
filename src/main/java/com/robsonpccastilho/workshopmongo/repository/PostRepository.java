882bf40965da7c9d96154d543d2cf67991e4d000
Robson Castilho <estudo@rcastilho.com.br>
2020-07-23 12:20:14 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/robsonpccastilho/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.robsonpccastilho.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.robsonpccastilho.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
