a743f3627cb0ec7476bd95e408beb8c88e459677
Robson Castilho <estudo@rcastilho.com.br>
2020-07-23 14:21:16 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/robsonpccastilho/workshopmongo/domain/Post.java
+++ src/main/java/com/robsonpccastilho/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.robsonpccastilho.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.robsonpccastilho.workshopmongo.dto.AuthorDTO;
+import com.robsonpccastilho.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -79,6 +84,14 @@ public class Post implements Serializable {
 		return result;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
